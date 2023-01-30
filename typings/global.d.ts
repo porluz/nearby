6450c1e09eb38b51eead396f070de995da6d07f1
Anthony Olivence <anthony.olivence@gmail.com>
2023-01-30 15:57:34 -0500

    Remove unnecessary typings

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -6,13 +6,3 @@ declare module "@emotion/react" {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface Theme extends ExactTheme {}
 }
-
-declare module "googlemaps";
-
-export {};
-
-declare global {
-  interface Window {
-    google: typeof google;
-  }
-}
