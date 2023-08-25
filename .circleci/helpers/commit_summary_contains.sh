4922045d88c16364dbec2ae9d87b06fddc1cd6af
Anthony Olivence <anthony.olivence@hyperscience.com>
2023-08-24 22:37:40 -0400

    Remove flags

--- .circleci/helpers/commit_summary_contains.sh
+++ .circleci/helpers/commit_summary_contains.sh
@@ -2,11 +2,11 @@
 # Check the last commit message for "skipTestCoverage"
 
 # Exit script if you try to use an uninitialized variable.
-set -o nounset
+# set -o nounset
 # Exit script if a statement returns a non-true return value.
-set -o errexit
+# set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
-set -o pipefail
+# set -o pipefail
 
 if [ $# -eq 0 ]; then
   echo "Error: Please provide the query string as an argument."
--- .circleci/helpers/pr_description_contains.sh
+++ .circleci/helpers/pr_description_contains.sh
@@ -2,11 +2,11 @@
 # Check if the PR description contains a query string
 
 # Exit script if you try to use an uninitialized variable.
-set -o nounset
+# set -o nounset
 # Exit script if a statement returns a non-true return value.
-set -o errexit
+# set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
-set -o pipefail
+# set -o pipefail
 
 # Check if the PR API URL and query string arguments are provided
 if [ $# -ne 2 ]; then
