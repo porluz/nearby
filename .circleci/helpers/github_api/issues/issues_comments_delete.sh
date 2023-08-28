8bfcf0d2c47bb9ece15b984e8110ad3649767f8a
Anthony Olivence <anthony.olivence@hyperscience.com>
2023-08-28 15:08:54 -0400

    Update scripts136

--- .circleci/helpers/github_api/issues/issues_comments_delete.sh
+++ .circleci/helpers/github_api/issues/issues_comments_delete.sh
@@ -15,6 +15,8 @@ COMMENT_ID="$2"
 
 comment_endpoint="$ISSUE_API_ENDPOINT/comments/$COMMENT_ID"
 
+echo "comment_endpoint: $comment_endpoint"
+
 payload="@./temp.json"
 authHeader="Authorization:Bearer $GITHUB_TOKEN"
 contentTypeHeader="Content-Type:application/json"
