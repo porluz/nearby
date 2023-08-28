7e1b88405d870b22bc7a65a40cbe2118f28e99b9
Anthony Olivence <anthony.olivence@hyperscience.com>
2023-08-28 14:36:49 -0400

    Update scripts130

--- .circleci/helpers/github_api/issues/issues_comments_get.sh
+++ .circleci/helpers/github_api/issues/issues_comments_get.sh
@@ -2,8 +2,6 @@
 
 # Exit script if you try to use an uninitialized variable.
 set -o nounset
-# Exit script if a statement returns a non-true return value.
-#set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
@@ -13,9 +11,10 @@ if [ $# -ne 1 ]; then
 fi
 ISSUE_API_ENDPOINT="$1"
 
+comments_url_endpoint="$ISSUE_API_ENDPOINT/comments"
 authHeader="Authorization:Bearer $GITHUB_TOKEN"
 contentTypeHeader="Content-Type:application/json"
-curl_command=(-X GET "$ISSUE_API_ENDPOINT" -H "$contentTypeHeader" -H "$authHeader")
+curl_command=(-X GET "$comments_url_endpoint" -H "$contentTypeHeader" -H "$authHeader")
 
 # Execute the curl command and store the response code
 response=$(curl "${curl_command[@]}")
